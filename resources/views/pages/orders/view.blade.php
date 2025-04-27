630fe2c37de0cc3e3509c85b83712a9b53cfe982
abdulaziz27 <doelziez212@gmail.com>
2025-04-27 15:54:26 +0700

    update: order view

--- resources/views/pages/orders/view.blade.php
+++ resources/views/pages/orders/view.blade.php
@@ -3,7 +3,6 @@
 @section('title', 'Order Detail')
 
 @push('style')
-    <!-- CSS Libraries -->
     <link rel="stylesheet" href="{{ asset('library/selectric/public/selectric.css') }}">
 @endpush
 
@@ -12,65 +11,64 @@
         <section class="section">
             <div class="section-header">
                 <h1>Order Detail</h1>
-
                 <div class="section-header-breadcrumb">
                     <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
                     <div class="breadcrumb-item"><a href="#">Orders</a></div>
                     <div class="breadcrumb-item">Order Detail</div>
                 </div>
             </div>
+
             <div class="section-body">
+                @include('layouts.alert')
+
                 <div class="row">
                     <div class="col-12">
-                        @include('layouts.alert')
-                    </div>
-                </div>
-                <h2 class="section-title">Order Detail</h2>
-                <p class="section-lead">
-                <div>Total Price {{ $order->total_price }}</div>
-                <div>Transaction Time {{ $order->transaction_time }}</div>
-                <div>Total Item {{ $order->total_item }}</div>
-                </p>
-
-
-                <div class="row mt-4">
-                    <div class="col-12">
-                        <div class="card">
+                        <div class="card shadow-sm">
                             <div class="card-header">
-                                <h4>All Products</h4>
+                                <h4>Order Summary</h4>
                             </div>
                             <div class="card-body">
 
-                                <div class="clearfix mb-3"></div>
+                                <div class="row mb-4">
+                                    <div class="col-md-4">
+                                        <div class="mb-2 font-weight-bold">Total Price:</div>
+                                        <div>{{ number_format($order->total_price, 0, ',', '.') }}</div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="mb-2 font-weight-bold">Transaction Time:</div>
+                                        <div>{{ \Carbon\Carbon::parse($order->transaction_time)->format('d F Y H:i:s') }}
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="mb-2 font-weight-bold">Total Items:</div>
+                                        <div>{{ $order->total_item }}</div>
+                                    </div>
+                                </div>
+
+                                <hr>
+
+                                <h5 class="mb-3">Products</h5>
 
                                 <div class="table-responsive">
-                                    <table class="table-striped table">
-                                        <tr>
-                                            <th>Product Name</th>
-                                            <th>Price</th>
-                                            <th>Quantity</th>
-                                            <th>Total Price</th>
-                                        </tr>
-                                        @foreach ($orderItems as $item)
+                                    <table class="table table-striped table-bordered">
+                                        <thead class="thead-light">
                                             <tr>
-
-                                                <td>{{ $item->product->name }}</td>
-                                                </td>
-                                                <td>
-                                                    {{ $item->product->price }}
-                                                </td>
-                                                <td>
-                                                    {{ $item->quantity }}
-                                                </td>
-                                                <td>
-                                                    {{ $item->total_price }}
-
-                                                </td>
-
+                                                <th>Wisata</th>
+                                                <th>Tiket</th>
+                                                <th>Price</th>
+                                                <th>Quantity</th>
                                             </tr>
-                                        @endforeach
-
-
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($orderItems as $item)
+                                                <tr>
+                                                    <td>{{ $item->product->category->name ?? '-' }}</td>
+                                                    <td>{{ $item->product->name }}</td>
+                                                    <td>{{ number_format($item->product->price, 0, ',', '.') }}</td>
+                                                    <td>{{ $item->quantity }}</td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
                                     </table>
                                 </div>
 
@@ -78,15 +76,13 @@
                         </div>
                     </div>
                 </div>
+
             </div>
         </section>
     </div>
 @endsection
 
 @push('scripts')
-    <!-- JS Libraies -->
     <script src="{{ asset('library/selectric/public/jquery.selectric.min.js') }}"></script>
-
-    <!-- Page Specific JS File -->
     <script src="{{ asset('js/page/features-posts.js') }}"></script>
 @endpush
